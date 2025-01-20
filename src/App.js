a8dbee8bb4e5a1a7ac6b8e0e6f7f034f9440d39a
uditrajmr3 <csense2653@gmail.com>
2025-01-20 15:15:41 +0530

    --update: modified useState with useReducer for better and cleaner code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import DateCounter from "./Components/DateCounter";
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello React!!</h1>
+      <DateCounter />
     </div>
   );
 }
